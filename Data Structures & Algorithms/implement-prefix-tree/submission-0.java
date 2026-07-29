26c76be2c26df423df998f03a349ab1533fc0922
Srinja Banerjee <89794694+srinja02@users.noreply.github.com>
2026-07-29 22:01:43 +0530

    Add: implement-prefix-tree - submission-0

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-0.java
@@ -0,0 +1,42 @@
+class PrefixTree {
+    private TrieNode root;
+    public PrefixTree() {
+         root = new TrieNode();
+    }
+
+    public void insert(String word) {
+        TrieNode cur = root;
+        for (char c : word.toCharArray()) {
+            cur.children.putIfAbsent(c, new TrieNode());
+            cur = cur.children.get(c);
+        }
+        cur.endOfWord = true;
+    }
+
+    public boolean search(String word) {
+        TrieNode cur = root;
+        for (char c : word.toCharArray()) {
+            if (!cur.children.containsKey(c)) {
+                return false;
+            }
+            cur = cur.children.get(c);
+        }
+        return cur.endOfWord;
+    }
+
+    public boolean startsWith(String prefix) {
+         TrieNode cur = root;
+        for (char c : prefix.toCharArray()) {
+            if (!cur.children.containsKey(c)) {
+                return false;
+            }
+            cur = cur.children.get(c);
+        }
+        return true;
+    }
+}
+
+public class TrieNode {
+    HashMap<Character, TrieNode> children = new HashMap<>();
+    boolean endOfWord = false;
+}
